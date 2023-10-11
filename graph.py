f97de18439936561b862f039418918ecfde7c91f
Thomas Nivoix <thomas.nivoix@etud.univ-jfc.fr>
2023-10-11 22:17:43 +0200

    Fix astropy bug, library removed

--- graph.py
+++ graph.py
@@ -8,7 +8,6 @@ matplotlib.use("TkAgg")
 
 # Libs for stats
 from scipy.stats import circmean, circvar
-from astropy.stats import rayleightest
 
 from static import Root, Side, Color
 
@@ -99,7 +98,7 @@ class CircularGraph:
             Z = n * r * r
             self.stats["Rayleigh test (Z)"] = Z
             # Rayleigh test p
-            p = rayleightest(self.data[label])
+            p = self.rayleightest(self.data[label])
             self.stats["Rayleigh test (p)"] = p
             # Rao's spacing test U
             u = 0
@@ -112,7 +111,39 @@ class CircularGraph:
                 u += abs(t - 2 * np.pi / n)
             u = 0.5 * u
             self.stats["Rao's spacing test (U)"] = u
+    
+    def rayleightest(self, data, axis=None, weights=None):
+        n = np.size(data, axis=axis)
+        Rbar = self._length(data, 1, 0.0, axis, weights)
+        z = n * Rbar * Rbar
 
+        # see [3] and [4] for the formulae below
+        tmp = 1.0
+        if n < 50:
+            tmp = (
+                1.0
+                + (2.0 * z - z * z) / (4.0 * n)
+                - (24.0 * z - 132.0 * z**2.0 + 76.0 * z**3.0 - 9.0 * z**4.0)
+                / (288.0 * n * n)
+            )
+
+        p_value = np.exp(-z) * tmp
+        return p_value
+
+    def _length(self, data, p=1, phi=0.0, axis=None, weights=None):
+        # Utility function for computing the generalized rectangular components
+        # of the circular data.
+        if weights is None:
+            weights = np.ones((1,))
+        try:
+            weights = np.broadcast_to(weights, data.shape)
+        except ValueError:
+            raise ValueError("Weights and data have inconsistent shape.")
+
+        C = np.sum(weights * np.cos(p * (data - phi)), axis) / np.sum(weights, axis)
+        S = np.sum(weights * np.sin(p * (data - phi)), axis) / np.sum(weights, axis)
+
+        return np.hypot(S, C)
 # Create the figure to display all graphs
 def setupFigure(name):
     nbRows = 2
